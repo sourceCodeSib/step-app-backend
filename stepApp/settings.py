26bc20574d2b6b61a7d399c27ce33bc0e9c4e498
sourceCodeSib <111237141+sourceCodeSib@users.noreply.github.com>
2024-04-01 23:13:43 +0700

    Update settings.py

--- stepApp/settings.py
+++ stepApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6yp43m8)d*fbh+uo7+^hc*2o1%$k(daxecljax2vyzgo(^u0j&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
